87ab1c512413dec1605bfee92dfeea2232ee3af4
Diego Malta <diego_malta@hotmail.com>
2024-01-28 00:00:44 -0600

    initial

---
+++ teamsBot.ts
@@ -0,0 +1,34 @@
+import {
+  TeamsActivityHandler,
+  CardFactory,
+  TurnContext,
+  AdaptiveCardInvokeValue,
+  AdaptiveCardInvokeResponse,
+} from "botbuilder";
+
+export class TeamsBot extends TeamsActivityHandler {
+  constructor() {
+    super();
+    this.onMessage(async (context, next) => {
+      console.log("Running with Message Activity.");
+      const removedMentionText = TurnContext.removeRecipientMention(context.activity);
+      const txt = removedMentionText.toLowerCase().replace(/\n|\r/g, "").trim();
+      await context.sendActivity(`Echo: ${txt}`);
+      // By calling next() you ensure that the next BotHandler is run.
+      await next();
+    });
+
+    this.onMembersAdded(async (context, next) => {
+      const membersAdded = context.activity.membersAdded;
+      for (let cnt = 0; cnt < membersAdded.length; cnt++) {
+        if (membersAdded[cnt].id) {
+          await context.sendActivity(
+            `Hi there! I'm a Teams bot that will echo what you said to me.`
+          );
+          break;
+        }
+      }
+      await next();
+    });
+  }
+}
